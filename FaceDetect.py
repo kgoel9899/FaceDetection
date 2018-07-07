e86c4e75751a096442b0b23fe956539a252e932d
Kshitij Goel <kshitijg9899@gmail.com>
2018-07-07 16:04:26 +0530

    Modified

--- FaceDetect.py
+++ FaceDetect.py
@@ -1,16 +1,25 @@
 import numpy as np
 import cv2
-path = "haarcascade_frontalface_default.xml"
-face_cascade = cv2.CascadeClassifier(path)
+path1 = "haarcascade_frontalface_default.xml"
+path2 = "haarcascade_eye.xml"
+face_cascade = cv2.CascadeClassifier(path1)
+eye_cascade = cv2.CascadeClassifier(path2)
 cap = cv2.VideoCapture(0)
 while(True):
     ret, frame = cap.read()
     frame = cv2.resize(frame, (0,0), fx = 0.5, fy = 0.5)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow("Face Detect", frame)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.10, minNeighbors=5, minSize=(40,40))
-    for (x, y, w, h) in faces:
+    face = face_cascade.detectMultiScale(gray, scaleFactor=1.10, minNeighbors=5, minSize=(40,40))
+    eye = eye_cascade.detectMultiScale(gray, scaleFactor=1.02, minNeighbors=20, minSize=(10,10))
+    for (x, y, w, h) in face:
         cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+    cv2.imshow("Face Detect", frame)
+    for (x, y, w, h) in eye:
+        xc = (x + x+w)/2
+        yc = (y + y+h)/2
+        radius = w/2
+        cv2.circle(frame, (int(xc),int(yc)), int(radius), (0,255,0), 2);
     cv2.imshow("Face Detect", frame)
     ch = cv2.waitKey(1)
     if (ch & 0xFF == ord('q')):
